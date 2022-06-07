a91c6c9392fd1aa39bfa2bbb1a068eee1f5c1bc7
fhidesjoseph <fhidesjoseph@gmail.com>
2022-06-07 13:15:11 +0300

    commit tree ee2e5d8bdbcc067f49999110dbeaab9d3896203c

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+//function index.js()
+//beforeEachfunction () {
+   // cats.length = 0;
+   // cats.push("Milo", "Otis", "Garfield");
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat() {
+  cats.push('Ralph');
+}
+function destructivelyPrependCat(name) {
+  cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat() {
+  cats.splice(-1);
+}
+function destructivelyRemoveFirstCat() {
+  cats.splice(0, 1);
+}
+function appendCat(name) {
+  const newcats = cats.slice();
+  newcats.push('Broom');
+  return newcats;
+}
+function prependCat(name) {
+  const newcats = cats.slice();
+  newcats.unshift('Arnold');
+  return newcats;
+}
+function removeLastCat() {
+  const newcats = cats.slice();
+  newcats.splice(-1);
+  return newcats;
+}
+function removeFirstCat() {
+  const newcats = cats.slice();
+  newcats.splice(0, 1);
+  return newcats;
+}
